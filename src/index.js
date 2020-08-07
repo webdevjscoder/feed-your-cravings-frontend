ba30b452ce61ec6bddddffd66982240c0f76ccd8
webdevjscoder <cobianjuan19@gmail.com>
2020-08-07 11:08:14 -0700

    create new instances in the frontend

--- src/index.js
+++ src/index.js
@@ -1,53 +1,135 @@
 const BASE_URL = 'http://localhost:3000';
 const MEALS_URL = `${BASE_URL}/meals`;
 const CATEGORIES_URL = `${BASE_URL}/categories`;
+const categoriesContainer = document.querySelector('#category-container');
+const categoryForm = document.querySelector('#category-form');
+const addMealBtn = document.querySelector('#add-new-meal');
+const mealContainer = document.querySelector('#meal-container');
+let addMeal = false;
 
-function fetchAllCategoriesAndMeals() {
+addMealBtn.addEventListener("click", () => {
+    addMeal = !addMeal;
+    if (addMeal) {
+        categoriesContainer.style.display = "block";
+        submitNewMeal();
+    } else {
+        categoriesContainer.style.display = "none";
+    }
+});
+
+function fetchAllCategories() {
     fetch(CATEGORIES_URL)
-        .then(function(response) {
-            return response.json()
+        .then(function(res) {
+            return res.json()
         })
-        .then(function(object) {
-            object.data.forEach(function(category) {
-                document.querySelector('.category-container').innerHTML += `
-                        <div class='menu-list' data-id="${category.id}">
-                            <a id='button' href='#'>${category.attributes.name}</a>
-                            <div class='meal-title'>
-                            </div>
-                        </div>
-                `
-            })
-            object.included.forEach(function(meal) {
-                document.querySelector(`[data-id="${meal.relationships.category.data.id}"]`).querySelector('#button').addEventListener('click', function(e) {
-                    e.preventDefault();
-                    const mealTitle = document.querySelector(`[data-id="${meal.relationships.category.data.id}"]`).querySelector('.meal-title');
-                    if (mealTitle) {
-                        mealTitle.style.display = 'block'; 
-                        mealTitle.innerHTML += `
-                            <ul class='meal' data-meal-id="${meal.id}">
-                                <li>${meal.attributes.name}</li>
-                            </ul>
-                        `
-                    }
-                    mealTitle.querySelector(`[data-meal-id="${meal.id}"]`).querySelector('li').addEventListener('click', function(e) {
-                        e.preventDefault();
-                        const mealContainer = document.querySelector('.meal-container')
-                        mealContainer.innerHTML += `
-                            <div class='meal-card'>
-                                <ul>
-                                    <li>${meal.attributes.name}</li>
-                                    <li>${meal.attributes.description}</li>
-                                    <li>${meal.attributes.price}</li>
-                                </ul>
-                            </div>
-                        `
-                    })
-                })
-            })
+        .then(function(categoryObj) {
+            // console.log(categoryObj)
+            createMenu(categoryObj)
         })
-        .catch(errors => alert(errors));
+        .catch(errors => alert(errors))
 }
 
-document.addEventListener("DOMContentLoaded", function (){
-    fetchAllCategoriesAndMeals()
-});
+function createMenu(categoryObj) {
+    // console.log(categoryObj)
+    categoryObj.data.forEach(function(category) {
+        // console.log(categoryObj)
+        // console.log(category)
+        mealContainer.innerHTML += `
+            <div class="category" data-category-id="category-${category.id}">
+                <a href="#">${category.attributes.name}</a>
+                <ul class="meals">
+                </ul>
+            </div>
+        `
+    })
+    categoryObj.included.forEach(function(meal)  {
+        const mealLink = document.querySelector(`[data-category-id="category-${meal.relationships.category.data.id}"]`).querySelector('a')
+        // console.log(mealLink)
+        mealLink.addEventListener('click', (e) => {
+            e.preventDefault();
+            const ul = document.querySelector(`[data-category-id="category-${meal.relationships.category.data.id}"]`).querySelector('.meals')
+            addMeal = !addMeal;
+            if (addMeal) {
+              ul.style.display = "block";
+              ul.innerHTML += `
+                <li>${meal.attributes.name}</li>
+                <li>${meal.attributes.description}</li>
+                <li>$${meal.attributes.price}</li>
+            `
+            } else {
+              ul.style.display = "none";
+            }
+        })
+    })
+    
+}
+
+function postCategory(data) {
+    configurationObject = {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          "Accept": "application/json"
+        },
+        body: JSON.stringify(data)
+    };
+    fetch(CATEGORIES_URL, configurationObject)
+        .then(function(res) {
+            return res.json();
+        })
+        .then(function(obj) {
+            // console.log(obj)
+            if (!!obj.data) {
+                debugger;
+                mealContainer.innerHTML += `
+                    <div class="category" data-category-id="category-${obj.data.id}">
+                        <a href="#">${obj.data.attributes.name}</a>
+                        <ul class="meals">
+                        </ul>
+                    </div>
+                `
+            } else {
+                alert(obj.message)
+            }
+        })
+        .catch(errors => alert(errors))
+}
+
+function formData() {
+    return {
+        name: categoryForm.name.value
+    };
+};
+
+function submitNewMeal() {
+    categoryForm.addEventListener('submit', (e) => {
+        e.preventDefault();
+        // console.log(e.target)
+        postCategory(formData());
+    })
+}
+
+// function createMenu(categoriesObj) {
+//     // console.log(categoriesObj.included)
+//     categoriesObj.data.forEach(function(category) {
+//         // console.log(category)
+//         categoriesContainer.innerHTML += `
+//             <div class='menu-list' data-id="category-${category.id}">
+//                 <p>${category.attributes.name}</p>
+//                 <ul id="category-${category.id}-meals">
+//                 </ul>
+//             </div>
+//         `
+//     })
+//     categoriesObj.included.forEach(function(meal) {
+//         // console.log(meal)
+//         document.querySelector(`[data-id="category-${meal.relationships.category.data.id}"]`).querySelector('p')
+//             let mealList = document.querySelector(`[data-id="category-${meal.relationships.category.data.id}"]`).querySelector(`[id="category-${meal.relationships.category.data.id}-meals"]`)
+//             // console.log(mealList)
+//                 mealList.innerHTML += `
+//                     <li><p>${meal.attributes.name}</p></li>
+//                 `
+//         })
+// }
+
+fetchAllCategories();
